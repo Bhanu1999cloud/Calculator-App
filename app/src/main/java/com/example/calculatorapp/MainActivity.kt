770cc3d6ed5c183b2a1945ea171e07c2d25f0fd6
Bhanu Bisht <bishtbhavana1234@gmail.com>
2020-07-20 12:22:32 +0530

    Sharing Calculator App

---
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -0,0 +1,121 @@
+package com.example.calculatorapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.ArithmeticException
+
+class MainActivity : AppCompatActivity() {
+    var lastNumeric=false
+    var lastDot = false
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+        fun onDigit(view: View) {
+            tvInput.append((view as Button).text)
+            lastNumeric=true
+
+        }
+
+        fun onClear(view: View) {
+            tvInput.text = ""
+            lastNumeric=false
+            lastDot=false
+        }
+
+        fun onDec(view: View) {
+            if (lastNumeric && !lastDot) {
+                tvInput.append(".")
+                lastNumeric = false
+                lastDot = true
+
+            }
+        }
+    fun onEqual(view: View) {
+        if (lastNumeric) {
+            var value = tvInput.text.toString()
+            var prefix = ""
+            try {
+                if (value.startsWith("-")) {
+                    prefix = "-"
+                    value = value.substring(1);
+                }
+                if (value.contains("/")) {
+                    val splitedValue = value.split("/")
+                    var one = splitedValue[0]
+                    val two = splitedValue[1]
+                    if (!prefix.isEmpty()) {
+                        one = prefix + one
+                    }
+                    tvInput.text = removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
+                } else if (value.contains("*")) {
+                    val splitedValue = value.split("*")
+                    var one = splitedValue[0]
+                    val two = splitedValue[1]
+                    if (!prefix.isEmpty()) {
+                        one = prefix + one
+                    }
+                    tvInput.text = removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
+                } else if (value.contains("-")) {
+                    val splitedValue = value.split("-")
+                    var one = splitedValue[0]
+                    val two = splitedValue[1]
+                    if (!prefix.isEmpty()) {
+                        one = prefix + one
+                    }
+                    tvInput.text = removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
+                } else if (value.contains("+")) {
+                    val splitedValue = value.split("+")
+                    var one = splitedValue[0]
+                    val two = splitedValue[1]
+
+                    if (!prefix.isEmpty()) {
+                        one = prefix + one
+                    }
+
+                    tvInput.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
+                }
+            } catch (e: ArithmeticException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+            fun onOperator(view: View)
+            {
+                if(lastNumeric && !isOperatorAdded(tvInput.text.toString()))
+                {
+                    tvInput.append((view as Button).text)
+                    lastNumeric=false
+                    lastDot=false
+                }
+            }
+
+    private fun isOperatorAdded(value: String):Boolean
+    {
+        return if(value.startsWith("-"))
+        {
+            false
+        }
+        else
+        {
+            value.contains("/")||value.contains("*")||value.contains("+")||value.contains("-")
+        }
+    }
+    private fun removeZeroAfterDot(result: String): String {
+
+        var value = result
+
+        if (result.contains(".0")) {
+            value = result.substring(0, result.length - 2)
+        }
+
+        return value
+    }
+    }
+
